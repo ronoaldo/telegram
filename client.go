20f15f61f114e4ab08f8981e9d19378d68c2f27a
Ronoaldo JLP <ronoaldo@gmail.com>
2016-10-03 01:03:42 +0000

    Fixed an issue when downloading a file.

--- client.go
+++ client.go
@@ -6,58 +6,88 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 const (
-	TelegramBotEndpoint = "https://api.telegram.org/bot"
+	TelegramBotEndpoint          = "https://api.telegram.org/bot"
 	TelegramFileDownloadEndpoint = "https://api.telegram.org/file/bot"
 )
+
+type DebugFunc func(msg string)
 
 type ApiClient struct {
 	client *http.Client
 	token  string
+	debug  DebugFunc
+}
+
+func stderrDebug(msg string) {
+	log.Println(msg)
 }
 
 // NewApiClient returns an instance of a Telegram Bot API client.
 func NewApiClient(c *http.Client, token string) *ApiClient {
 	return &ApiClient{
 		client: c,
-		token: token,
+		token:  token,
+		debug:  stderrDebug,
+	}
+}
+
+func (t *ApiClient) Debug(dbg DebugFunc) {
+	t.debug = dbg
+}
+
+func (t *ApiClient) Debugf(msg string, args ...interface{}) {
+	if t.debug != nil {
+		t.debug(fmt.Sprintf(msg, args...))
 	}
 }
 
 func (t *ApiClient) Call(httpMethod, apiMethod string, in, out interface{}) error {
 	var buff bytes.Buffer
-	
+
 	if err := json.NewEncoder(&buff).Encode(in); err != nil {
 		return err
 	}
-	
+
 	url := fmt.Sprintf("%s%s/%s", TelegramBotEndpoint, t.token, apiMethod)
-	
+
 	req, err := http.NewRequest(httpMethod, url, &buff)
 	if err != nil {
 		return err
 	}
-	
+
 	req.Header.Set("Content-Type", "application/json")
-	
+
 	resp, err := t.client.Do(req)
 	if err != nil {
 		return err
-	}	
+	}
 	defer resp.Body.Close()
-	
+
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	
+	t.Debugf("* API Response: %v", string(b))
+
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return fmt.Errorf("telegram: unexpected response: %v: %v [%s]", resp.StatusCode, resp.Status, string(b))
 	}
-	return json.Unmarshal(b, out)
+
+	// Check if operation suceeded
+	apiResp := new(ApiResponse)
+	if err := json.Unmarshal(b, apiResp); err != nil {
+		return fmt.Errorf("telegram: unable to parse response %v", err)
+	}
+	if !apiResp.OK {
+		return fmt.Errorf("telegram: operation failed: %s", string(b))
+	}
+
+	return json.Unmarshal(apiResp.Result, out)
 }
 
 // GetFile prepares a file for download.
@@ -65,6 +95,7 @@ func (t *ApiClient) GetFile(fileId string) (*File, error) {
 	params := map[string]string{
 		"file_id": fileId,
 	}
+	t.Debugf("Fetching file with getFile: %v", fileId)
 	file := new(File)
 	if err := t.Call("GET", "getFile", params, file); err != nil {
 		return nil, err
@@ -92,7 +123,7 @@ func (t *ApiClient) DownloadFile(f *File, w io.Writer) error {
 func (t *ApiClient) SendMessage(to, text string) (*Message, error) {
 	params := map[string]string{
 		"chat_id": to,
-		"text": text,
+		"text":    text,
 	}
 	msg := new(Message)
 	if err := t.Call("POST", "sendMessage", params, msg); err != nil {
@@ -102,7 +133,7 @@ func (t *ApiClient) SendMessage(to, text string) (*Message, error) {
 }
 
 // SendMessagef calls fmt.Sprintf and passes the resulting message to SendMessage.
-func (t *ApiClient) SendMessagef(to, formatText string, args...interface{}) (*Message, error) {
+func (t *ApiClient) SendMessagef(to, formatText string, args ...interface{}) (*Message, error) {
 	return t.SendMessage(to, fmt.Sprintf(formatText, args...))
 }
 
@@ -113,4 +144,11 @@ func (t *ApiClient) SetWebhook(httpsURL string) error {
 		return err
 	}
 	return nil
+}
+
+// ApiResponse is the response API wrapper.
+type ApiResponse struct {
+	OK      bool            `json:"ok"`
+	Result  json.RawMessage `json:"result"`
+	Message string          `json:"message"`
 }
